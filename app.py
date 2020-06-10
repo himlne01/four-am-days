2ba6b5fecc00e8d9211de7eb9aae265e5ec844a4
himlne01 <himlne01@luther.edu>
2020-06-10 00:57:05 -0500

    delete pexels and sqlite3

--- app.py
+++ app.py
@@ -2,29 +2,29 @@ import psycopg2
 import os
 from flask import Flask, render_template, request
 import sqlite3
-from pexels_api import API
+# from pexels_api import API
 
 app = Flask(__name__)
 SECRET_KEY = os.environ.get("SECRET_KEY")
-PEXELS_API_KEY = os.environ.get('YOUR_API_KEY')
+# PEXELS_API_KEY = os.environ.get('YOUR_API_KEY')
 
-def getPhoto(query: str):
-    api = API(PEXELS_API_KEY)
-    api.search(query, page=12, results_per_page=1)
-    photos = api.get_entries()
-    for photo in photos:
-        return(photo.small)
+# def getPhoto(query: str):
+#     api = API(PEXELS_API_KEY)
+#     api.search(query, page=12, results_per_page=1)
+#     photos = api.get_entries()
+#     for photo in photos:
+#         return(photo.small)
 
 def get_data_from_db(query: str) -> list:
-    conn = sqlite3.connect('day_tabase.sqlite3')
+    # conn = sqlite3.connect('day_tabase.sqlite3')
 
-    # conn = psycopg2.connect(
-    #     database = "dgju4sm6llfrr",
-    #     user="tlcqqkcatmpplu",
-    #     password=SECRET_KEY,
-    #     host="ec2-34-200-72-77.compute-1.amazonaws.com",
-    #     port="5432"
-    # )
+    conn = psycopg2.connect(
+        database = "dgju4sm6llfrr",
+        user="tlcqqkcatmpplu",
+        password=SECRET_KEY,
+        host="ec2-34-200-72-77.compute-1.amazonaws.com",
+        port="5432"
+    )
     cur = conn.cursor()
     cur.execute(query)
     rows = cur.fetchall()
